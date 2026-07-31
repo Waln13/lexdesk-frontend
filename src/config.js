90a95b54931a14b1348ffa79aeba0e337c99440b
Walny Miguel Carreras Valencia <elwalny13@gmail.com>
2026-07-31 18:51:13 -0400

    fix: vite config build time

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+console.log("ENV:", import.meta.env.VITE_API_URL);
+console.log("MODE:", import.meta.env.MODE);
+
 const API = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
 
 export default API;
